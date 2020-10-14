900e70fe5abd5832018d25441888fa0d0c23dc96
atm <atm@atms-MacBook-Pro.local>
2020-10-15 02:26:46 +0700

    init

---
+++ public/index.js
@@ -0,0 +1,50 @@
+var Hello = {
+    connection: null,
+    log: function (msg) {
+        $('#log').append(" < p > " + msg + " < /p>");
+    }
+};
+$(document).ready(function () {
+    $('#login_dialog').dialog({
+        autoOpen: true,
+        draggable: false,
+        modal: true,
+        title: 'Connect to XMPP',
+        buttons: {
+            "Connect": function () {
+                $(document).trigger('connect', {
+                    jid: $('#jid').val(),
+                    password: $('#password').val()
+                });
+                $('#password').val('');
+                $(this).dialog('close');
+            }
+        }
+    });
+});
+$(document).bind('connect', function (ev, data) {
+    var conn = new Strophe.Connection(
+        "http://10.8.0.38:5443/bosh", {sync: true});
+    console.log(data.jid, data.password)
+    conn.connect(data.jid, data.password, function (status) {
+        console.log(status)
+        console.log(Strophe.Status)
+        if (status === Strophe.Status.CONNECTED) {
+            $(document).trigger('connected');
+        } else if (status === Strophe.Status.DISCONNECTED) {
+
+            $(document).trigger('disconnected');
+            console.log('disconnected')
+        }
+    },6000,3);
+    Hello.connection = conn;
+});
+$(document).bind('connected', function () {
+    // inform the user
+    Hello.log("Connection established.");
+});
+$(document).bind('disconnected', function () {
+    Hello.log("Connection terminated.");
+    // remove dead connection object
+    Hello.connection = null;
+});
